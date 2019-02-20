b1f5a1ceecc33788fd9a01158d5177c3f80f2637
Serghei Golovatii <Serghei.Golovatii@endava.com>
2019-02-20 15:37:11 +0200

    Add move left and move right for Chungus

--- play.py
+++ play.py
@@ -1,12 +1,4 @@
 import sys, pygame
-pygame.init()
-
-screenHeight, screenWidth = 1280, 720
-screen = pygame.display.set_mode((screenHeight, screenWidth))
-
-pygame.display.set_caption("Small py game demo")
-
-isGameRunning = True
 
 class Background(pygame.sprite.Sprite):
 	def __init__(self, image_file, location):
@@ -23,15 +15,52 @@ class Chungus(pygame.sprite.Sprite):
 		self.rect.left, self.rect.top = location
 		self.size = self.__image.get_size()
 		self.image = pygame.transform.scale(self.__image, (int(self.size[0]//4), int(self.size[1]//4)))
+		self._speed = 5
 
-while(isGameRunning):
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN and pygame.key.get_pressed()[pygame.K_ESCAPE]:
-			isGameRunning = False
+	def getSpeed(self):
+		return self._speed
 
+
+def main():
+	pygame.init()
+	
+	screenHeight, screenWidth = 1024, 768
+	screen = pygame.display.set_mode((screenHeight, screenWidth))
+
+	pygame.display.set_caption("Small py game demo")
+
+	isGameRunning = True
 	backgroundTropicalBeach = Background('resources/sprites/background_tropical_beach.jpg', [0, 0])
-	screen.fill([255, 255, 255])
-	screen.blit(backgroundTropicalBeach.image, backgroundTropicalBeach.rect)
+	startPoint = [0, 200]
+	chungus = Chungus('resources/sprites/chungus.jpg', startPoint)
 
-	chungus = Chungus('resources/sprites/chungus.jpg', [0, 0])
-	screen.blit(chungus.image, chungus.rect)
+	while(isGameRunning):
+
+		screen.fill([255, 255, 255])
+		screen.blit(backgroundTropicalBeach.image, backgroundTropicalBeach.rect)
+		screen.blit(chungus.image, chungus.rect)
+		pygame.display.update()
+
+		for event in pygame.event.get():
+			if (event.type == pygame.KEYDOWN and pygame.key.get_pressed()[pygame.K_ESCAPE]) or event.type == pygame.QUIT:
+				isGameRunning = False
+		
+		if (pygame.key.get_pressed()[pygame.K_RIGHT]):
+			screen.fill([255, 255, 255])
+			screen.blit(backgroundTropicalBeach.image, backgroundTropicalBeach.rect)
+			moveRightPos = chungus.rect
+			moveRightPos.right += chungus.getSpeed()
+			screen.blit(chungus.image, moveRightPos)
+			pygame.display.update()
+		elif (pygame.key.get_pressed()[pygame.K_LEFT]):
+			screen.fill([255, 255, 255])
+			screen.blit(backgroundTropicalBeach.image, backgroundTropicalBeach.rect)
+			moveLeftPos = chungus.rect
+			moveLeftPos.left -= chungus.getSpeed()
+			screen.blit(chungus.image, moveLeftPos)
+			pygame.display.update()
+
+
+main()
+
+
